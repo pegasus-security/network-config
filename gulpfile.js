a1737474b1f7801374b3196b82aea3b52291734f
syneder <67856698+syneder@users.noreply.github.com>
2021-10-19 07:33:04 +0300

    Added VLAN configuration converter

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ require('handlebars-helpers')();
 const __options = {};
 const __optionsSetter = function (chunk) {
   __options.data = json5.parse(chunk.contents.toString('utf-8'));
+  __options.data = __convertOptions(__options.data);
 };
 
 const compileHandlebars = function () {
@@ -50,3 +51,45 @@ gulp.task('compile::handlebars', async () => {
 });
 
 gulp.task('compile', gulp.series('compile::options', 'compile::handlebars'));
+
+const __convertOptions = function (options, context = {}) {
+  for (const [optionIndex, optionValue] of Object.entries(options)) {
+    if (optionIndex === 'vlans' && !_.isArray(optionValue)) {
+      options[optionIndex] = __convertVlans(optionValue, context);
+    }
+    else if (_.isObject(optionValue)) {
+      options[optionIndex] = __convertOptions(_.cloneDeep(optionValue), context);
+    }
+    else if (_.isString(optionValue)) {
+      options[optionIndex] = __convertString(optionValue, context);
+    }
+  }
+  return options;
+}
+
+const __convertVlans = function (options, context) {
+  const convertedVlans = [];
+  for (const [optionIndex, optionValue] of Object.entries(options)) {
+    const parsedIndex = _.toNumber(optionIndex);
+    let parsedIndexes = parsedIndex || optionIndex.match(/^(\d+)\.\.(\d+)$/)?.slice(1);
+
+    if (!!parsedIndexes) {
+      parsedIndexes = _.castArray(parsedIndexes);
+      for (let i = parsedIndexes[0], end = _.last(parsedIndexes); i <= end; i++) {
+        const vlanContext = _.extend({}, _.cloneDeep(context), { vlan: { id: i } });
+        convertedVlans[i] = __convertOptions(_.cloneDeep(optionValue), vlanContext);
+      }
+    }
+  }
+
+  return convertedVlans;
+};
+
+const __convertString = function (optionValue, context) {
+  return _.replace(optionValue, /\{\{(vlan|env)::([^\}]+)\}\}/g, (_, func, arg) => {
+    switch (func) {
+      case 'vlan': return context.hasOwnProperty(func) ? context[func][arg] : null;
+      case 'env': return process.env[arg];
+    }
+  });
+};
